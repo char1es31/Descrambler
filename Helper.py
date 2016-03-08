155413dd5179bf1b5acf2d8eaffa09d211c17c56
Charles Bellemare <charles.bellemare93@gmail.com>
2016-03-08 15:23:42 -0500

    Added get_langauges function
    Searches through the given directory for word-list and alphabet files of
    the same langauge and return a list of the languages available.

--- Helper.py
+++ Helper.py
@@ -1,3 +1,8 @@
+from os import listdir
+from os.path import isfile, join
+import re
+
+
 def get_words(word_list_file_name: str) -> list:
     """Returns a list of words found in the file"""
 
@@ -25,3 +30,29 @@ def get_alphabet(alphabet_file_name: str) -> list:
         alphabet.append(c)
 
     return alphabet
+
+
+def get_languages(directory_name: str) -> list:
+    """Returns a list of all availible languages in the LanguageAssets folder"""
+
+    language_list = []
+
+    file_list = [
+        f for f in listdir(directory_name) if isfile(join(directory_name, f))]
+
+    lang1, lang2 = "", ""
+
+    # scan for alphabet file
+    for file_name1 in file_list:
+        if re.match('alphabet.\w{2}.txt', file_name1):
+            # if found, scan for word-list file of the same language
+            lang1 = file_name1.replace('alphabet.', '').replace('.txt', '')
+
+            for file_name2 in file_list:
+                if re.match('word-list.' + lang1 + '.txt', file_name2):
+                    lang2 = file_name1.replace('alphabet.', '').replace('.txt', '')
+
+                    if lang1 == lang2:
+                        language_list.append(lang1)
+
+    return language_list
